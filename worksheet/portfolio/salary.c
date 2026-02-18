b245b5af2f6796df344db06960279e4c63f10f10
jpqr0294 <jpqr0294@leeds.ac.uk>
2026-02-18 18:31:28 +0000

    Worksheet 2.3

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,30 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Finley Schofield 
+ * ID: JPQR0294
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
+   float salary = 36250;   // In range 14000-40000
+   float niRate = 0.08;
+   float taxRate = 0.15;
+   int taxFree = 12500;
+   float takehome, nicont, taxcont;
 
     // calculate the deductions and final take-home salary
+   nicont = salary * niRate;
+   taxcont = (salary - taxFree) * taxRate;
+
+   takehome = salary - nicont - taxcont;
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%.2f\n",salary);
+    printf("NI contribution £%.2f\n",nicont);
+    printf("Tax contribution £%.2f\n",taxcont);
+    printf("Take home salary £%.2f\n",takehome);
 
     return 0;
  }
